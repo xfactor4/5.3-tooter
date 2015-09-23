dffa958ae986570e5bf85bb49f473f4c662400ed
Jacob Smith <jacob@jacobsmith.io>
2015-09-23 11:59:13 -0400

    Use model and view in application

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,12 @@ var views = require('views');
 
 $(document).ready(function(){
   $('body').prepend(JST.application());
+
+  var view = new PostsView();
+
+  $(document).on('posts:fetched', function(event, posts){
+    view.showPosts(posts);
+  });
+
+  Post.fetch();
 });
